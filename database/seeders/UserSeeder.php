47671624dd3d6e67377fd052297842765450a741
monasabry <monasabrykhafagi2018@gmail.com>
2024-01-31 13:10:40 +0200

    Add UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,14 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
-class TransectionSeeder extends Seeder
+use App\Models\User;
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+       User::create([
+        'email' => 'admin@gmail.com',
+        'password' => Hash::make('123456789'),
+       ]);
     }
 }
